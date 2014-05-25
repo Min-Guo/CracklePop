b2c706911bce5240ea6f68b67c69e3cf60910e93
Min Guo <xmguomin@gmail.com>
2014-05-24 22:29:24 -0400

    Rewrite the code

--- CracklePop.py
+++ CracklePop.py
@@ -3,16 +3,21 @@
 # May 22, 2014
 # Write a program that prints out the numbers 1 to 100 (inclusive). If the number is divisible by 3, print Crackle instead of the number. If it is divisible by 5, print Pop. if it is divisible by both 3 and 5, print CracklePop.
 
-def CracklePop():
 
+def cracklePop_100():
 	for num in range(1, 101):
- 		if (num % 3 == 0) and (num % 5 == 0):
- 			print "CracklePop"
- 		elif num % 3 == 0:
- 			print "Crackle"
- 		elif num % 5 == 0:
- 			print "Pop"
- 		else:
- 			print num
+		print cracklePop(num)
 
-CracklePop()
+def cracklePop(num):
+	if (num % 3 == 0) and (num % 5 == 0):
+		return "CracklePop"
+	elif num % 3 == 0:
+		return "Crackle"
+	elif num % 5 == 0:
+		return "Pop"
+	else:
+		return num
+
+cracklePop_100()
+
+
